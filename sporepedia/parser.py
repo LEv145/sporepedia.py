f3625dd0424d1de5983a45d34ebdc7cb5d72f90f
lev145 <levxxx03@gmail.com>
2021-11-29 23:44:47 +0200

    V0.21

---
+++ sporepedia/parser.py
@@ -0,0 +1,43 @@
+from dataclasses import dataclass
+from typing import Dict, Tuple
+
+from .errors import DwrParserError
+
+from js2py import EvalJs
+
+
+class SporeDwrEngineParser():
+    _extension_code = (
+        "outlog = null;\n"
+        "errorlog = null;\n"
+
+        "dwr = {};\n"
+        "dwr.engine = {};\n"
+
+        "dwr.engine._remoteHandleBatchException = function(exeption, batchId) {\n"
+        "    errorlog = exeption;\n"
+        "};\n"
+
+        "dwr.engine._remoteHandleCallback = function(batchId, callId, reply) {\n"
+        "    outlog = reply;\n"
+        "};\n"
+    )
+
+    def parse(self, text: str) -> Tuple[Dict[str, str], Dict[str, str]]:
+        js_code = text.replace("throw 'allowScriptTagRemoting is false.';", "")  # Remove throw
+
+        context = EvalJs()
+
+        context.execute(
+            (
+                f"{self._extension_code}"
+                f"{js_code}"
+            )
+        )
+
+        outlog, errorlog = context.outlog, context.errorlog
+
+        if errorlog is not None:
+            raise DwrParserError(message=errorlog["message"], name=errorlog["name"])
+
+        return outlog, errorlog
