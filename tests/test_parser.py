59169b578dbb21cee56d20732a2163fa40372549
lev145 <levxxx03@gmail.com>
2021-12-02 23:23:52 +0200

    V0.3.6

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,5 +1,6 @@
 from typing import cast
 import unittest
+from pathlib import Path
 
 from sporepedia.errors import DwrParserError
 from sporepedia.dwr_parser import SporeDwrEngineParser
@@ -7,18 +8,11 @@ from sporepedia.dwr_parser import SporeDwrEngineParser
 
 class SporeDwrEngineParsersTest(unittest.TestCase):
 
-    def test_exception(self):  # TODO: Name?
+    def test_exception(self):
         parser = SporeDwrEngineParser()
 
-        text = """throw 'allowScriptTagRemoting is false.';
-//#DWR-REPLY
-if (window.dwr) dwr.engine._remoteHandleBatchException({ \
-name:'org.directwebremoting.extend.ServerException', \
-message:'The specified call count is not a number' });
-else if (window.parent.dwr) window.parent.dwr.engine._remoteHandleBatchException({ \
-name:'org.directwebremoting.extend.ServerException', \
-message:'The specified call count is not a number' });
-"""
+        with open(Path("./tests/testdata/dwr_exception.js")) as fp:
+            text = fp.read()
 
         try:
             parser.parse(text)
@@ -30,47 +24,12 @@ message:'The specified call count is not a number' });
     def test_normal(self):
         parser = SporeDwrEngineParser()
 
-        text = """throw 'allowScriptTagRemoting is false.';
-//#DWR-INSERT
-//#DWR-REPLY
-var s3=[];var s1={};var s5={};var s6=[];var s0={};var s7={};\
-var s2={};var s8={};var s9=[];var s4={};var s10=[];s3[0]=s1;s3[1]=s2;
-s1.adventureStat=null;s1.assetFunction='CITY_HALL';s1.assetId=500586138735;\
-s1.auditTrail=null;s1.author=s5;s1.created=new Date(1273147355612);\
-s1.description="test, test, test. Kann mir mal einer sagen ob die Kreation online gestellt wurde?";\
-s1.featured=null;s1.id=500586138735;s1.imageCount=1;s1.localeString="de_DE";s1.name="Test";\
-s1.originalId=500586138735;s1.parentId=null;s1.quality=false;s1.rating=null;s1.requiredProducts=s6;\
-s1.sourceIp=null;s1.status=s0;s1.tags="hungersnot, test";s1.thumbnailSize=23637;\
-s1.type='BUILDING';s1.updated=new Date(1273147355597);
-s5.assetCount=1768;s5.avatarImage="thumb\\/500\\/470\\/438\\/500470438972.png";\
-s5.avatarImageCustom=false;s5.dateCreated=new Date(1214650140000);\
-s5['default']=true;s5.id=2263769540;s5.lastLogin=new Date(1397251419188);s5.name="Hungersnot";\
-s5.newestAssetCreated=new Date(1299428796992);s5.nucleusUserId=2263769540;s5.personaId=174473339;\
-s5.screenName="Hungersnot";s5.subscriptionCount=161;\
-s5.tagline="Einzugestehen dass man etwas nicht wei\u00DF ist Wissen";\
-s5.updated=new Date(1278487634765);s5.userId=2263769540;
-s6[0]='SPORE_CORE';
-s0.declaringClass=s7;s0.name="CLASSIFIED";s0.nameKey="asset.status.classified";
-s7.name="com.ea.sp.pollinator.db.Asset$Status";
-s2.adventureStat=null;s2.assetFunction='CITY_HALL';s2.assetId=500198868984;s2.auditTrail=null;\
-s2.author=s8;s2.created=new Date(1227979603997);s2.description="Color eye test, what is the answer?";\
-s2.featured=null;s2.id=500198868984;s2.imageCount=1;s2.localeString=null;s2.name="Color eye test";\
-s2.originalId=null;s2.parentId=500198771887;s2.quality=false;s2.rating=-1.0;s2.requiredProducts=s9;\
-s2.sourceIp=null;s2.status=s0;s2.tags="eye test";s2.thumbnailSize=22217;s2.type='BUILDING';\
-s2.updated=new Date(1227979603997);
-s8.assetCount=768;s8.avatarImage="thumb\\/500\\/655\\/246\\/500655246178.png";s8.avatarImageCustom=false;\
-s8.dateCreated=new Date(1213730460000);s8['default']=true;s8.id=2259901423;\
-s8.lastLogin=new Date(1606135213248);s8.name="DeKDeS";s8.newestAssetCreated=new Date(1420413732605);\
-s8.nucleusUserId=2259901423;s8.personaId=173794308;s8.screenName="DeKDeS";s8.subscriptionCount=246;\
-s8.tagline="No longer active";s8.updated=new Date(1433801298020);s8.userId=2259901423;
-s9[0]='SPORE_CORE';
-s4['class com.ea.sp.pollinator.db.Asset']=s10;
-s10[0]=s1;s10[1]=s2;
-dwr.engine._remoteHandleCallback('8','0',{resultSize:2846,results:s3,resultsPerType:s4});"""
+        with open(Path("./tests/testdata/dwr_search_testdata.js")) as fp:
+            text = fp.read()
 
         outlog = parser.parse(text)
 
-        self.assertEqual(  # TODO
+        self.assertEqual(  # TODO: How test JsObjectWrapper
             outlog.to_dict()["resultSize"],
-            2846
+            1
         )
---
+++ tests/testdata/dwr_search_testdata.js
@@ -0,0 +1,13 @@
+throw 'allowScriptTagRemoting is false.';
+//#DWR-INSERT
+//#DWR-REPLY
+var s1=[];var s0={};var s3={};var s4={};var s5=[];var s6={};var s7={};var s2={};var s8=[];s1[0]=s0;
+s0.adventureStat=s3;s0.assetFunction='ADV_PUZZLE';s0.assetId=500377997389;s0.auditTrail=null;s0.author=s4;s0.created=new Date(1246035348779);s0.description="A psychic entity has you at its disposal. What will it have you do? Now actually working! Thanks for making this a rising star guys.EDIT: I haven\'t checked out this in a while! Thanks for making this on the TOP PAGE! ";s0.featured=new Date(1247011200000);s0.id=500377997389;s0.imageCount=2;s0.localeString="en_US";s0.name="The Psychic Planet";s0.originalId=500377997764;s0.parentId=500377997764;s0.quality=true;s0.rating=14.376374;s0.requiredProducts=s5;s0.sourceIp="98.203.139.225";s0.status=s6;s0.tags="cool,fun,lava,psychic,puzzle,test";s0.thumbnailSize=41862;s0.type='ADVENTURE';s0.updated=new Date(1461858314548);
+s3.adventureId=500377997389;s3.adventureLeaderboardId=500377997389;s3.difficulty=5;s3.lockedCaptainAssetId=null;s3.losses=104177;s3.pointValue=51;s3.totalPlays=157748;s3.updated=new Date(1371579201795);s3.wins=53571;
+s4.assetCount=128;s4.avatarImage="thumb\/500\/335\/938\/500335938963.png";s4.avatarImageCustom=false;s4.dateCreated=new Date(1213754220000);s4['default']=true;s4.id=2262951433;s4.lastLogin=new Date(1337907125837);s4.name="Doomwaffle";s4.newestAssetCreated=new Date(1332451380361);s4.nucleusUserId=2262951433;s4.personaId=173842184;s4.screenName="Doomwaffle";s4.subscriptionCount=359;s4.tagline="Galactic Adventurer";s4.updated=new Date(1245989976725);s4.userId=2262951433;
+s5[0]='EXPANSION_PACK1';s5[1]='INSECT_LIMBS';s5[2]='SPORE_CORE';
+s6.declaringClass=s7;s6.name="CLASSIFIED";s6.nameKey="asset.status.classified";
+s7.name="com.ea.sp.pollinator.db.Asset$Status";
+s2['class com.ea.sp.pollinator.db.Asset']=s8;
+s8[0]=s0;
+dwr.engine._remoteHandleCallback('4','0',{resultSize:1,results:s1,resultsPerType:s2});
