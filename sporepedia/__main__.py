dca5bbb60f66762b6bda803e36386966c87760c7
lev145 <levxxx03@gmail.com>
2021-12-09 23:56:18 +0200

    V0.4dev-1

--- sporepedia/__main__.py
+++ sporepedia/__main__.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
+from dataclasses import is_dataclass
+from typing import TYPE_CHECKING, Dict, Optional, Type
 
 import asyncclick as click
+
 
 from sporepedia import (
     SporepediaClient,
@@ -13,53 +16,56 @@ from sporepedia import (
 )
 
 
+if TYPE_CHECKING:
+    from dataclasses import Field
+
+    from sporepedia import ABCSearchParam
+
+    from click import Context, Parameter
+
+
 _client = SporepediaClient()
+
+
+class BoolDatetimeType(click.ParamType):
+    def __init__(self, dataclass_: Type["ABCSearchParam"]):
+        if not is_dataclass(dataclass_):  # TODO?: Dataclass type?
+            raise ValueError(f"{dataclass_!r} is not dataclass")
+        self._dataclass = dataclass_
+
+    def convert(
+        self,
+        user_input: str,
+        parameter: Optional["Parameter"],
+        ctx: Optional["Context"]
+    ):
+        if user_input == "all":
+            return self._dataclass.all()
+
+        if user_input == "none":
+            return self._dataclass.none()
+
+        dataclass_fields: Dict[str, Field] = (
+            self._dataclass.__dataclass_fields__  # type: ignore
+        )
+
+        user_values = [
+            item
+            for i in user_input.split(",")
+            if (item := i.strip())
+        ]
+
+        dataclass_attrs = {}
+
+        for value in user_values:
+            if value not in dataclass_fields:
+                self.fail(f"{value!r} not in parametes [{', '.join(dataclass_fields.keys())}]")
+            else:
+                dataclass_attrs[value] = True
+
+        return self._dataclass(**dataclass_attrs)  # type: ignore
 
 
 @click.group()
 async def cli():
     """CLI for Spore REST API"""
-
-
-@cli.command(help="Search from sporepedia")  # TODO
-@click.argument(
-    "text",
-    type=str,
-    prompt="Enter search text",
-)
-@click.option(
-    "-Fu", "--functions",
-    type=click.Choice(
-        tuple(FunctionsSearchParam.__dataclass_fields__.keys())
-    ),
-    multiple=True,
-)
-@click.option(
-    "-Fi", "--fields",
-    type=click.Choice(
-        tuple(FieldsSearchParam.__dataclass_fields__.keys())
-    ),
-    multiple=True,
-)
-@click.option(
-    "-M", "--models",
-    type=click.Choice(
-        tuple(ModelsSearchParam.__dataclass_fields__.keys())
-    ),
-    multiple=True,
-)
-@click.option(
-    "-P", "--purposes",
-    type=click.Choice(
-        tuple(PurposesSearchParam.__dataclass_fields__.keys())
-    ),
-    multiple=True,
-)
-@click.option(
-    "-F", "--filter",
-    type=click.Choice(
-        tuple(SearchFilter._member_names_)
-    )
-)
-async def search(text: str, functions, fields, models, purposes, filter):
-    print(text, functions, fields, models, purposes, filter, sep="\n")
